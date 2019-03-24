9b09e4d77dbb2f75b77ed6ff3cab34ef1f923c81
zhy282289 <396962243@qq.com>
2019-03-24 22:57:42 +0800

    1

--- TouTiaoDownload/DownloadTaskWnd.cpp
+++ TouTiaoDownload/DownloadTaskWnd.cpp
@@ -438,7 +438,7 @@ void DownloadTaskWnd::GetAndRemoveFromDBTaskCount(int count)
 
 	for (auto info : infos)
 	{
-		//MY_DB->DownladRemove(info->id);
+		MY_DB->DownladRemove(info->id);
 		AddItem(info);
 	}
 }
--- TouTiaoDownload/main.cpp
+++ TouTiaoDownload/main.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "TouTiaoDownload.h"
 
-
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
